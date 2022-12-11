66fc9fcd3a25c59a269fe680e2f3ef25e33605ff
Ankesh Yadav <92791586+ankesh1111@users.noreply.github.com>
2022-12-11 16:17:28 +0530

    Updated

---
+++ Backend_App/config/db.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose")
+
+const connection = mongoose.connect("mongodb+srv://yadavji:yadavji@cluster0.pzqahii.mongodb.net/myseconddb?retryWrites=true&w=majority")
+
+module.exports = {
+    connection
+}
