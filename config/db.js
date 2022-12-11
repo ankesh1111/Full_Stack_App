6aefb78f0cd05a7fb44f64de84bfe433869e2f1a
Ankesh Yadav <92791586+ankesh1111@users.noreply.github.com>
2022-12-11 15:14:34 +0530

    idkjf

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 
-const connection = mongoose.connect("mongodb+srv://yadavji:yadavji@cluster0.pzqahii.mongodb.net/myfirstdb?retryWrites=true&w=majority")
+const connection = mongoose.connect("mongodb+srv://yadavji:yadavji@cluster0.pzqahii.mongodb.net/myseconddb?retryWrites=true&w=majority")
 
 
 module.exports = {
